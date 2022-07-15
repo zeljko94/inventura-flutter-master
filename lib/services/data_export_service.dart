681f173f4cd2427fc1dfa885542641440e97e3d0
Željko Krnjić <zeljkok@in2.hr>
2022-07-15 10:14:00 +0200

    export excel bug fixes

--- lib/services/data_export_service.dart
+++ lib/services/data_export_service.dart
@@ -39,8 +39,6 @@ class DataExportService {
       for (int i = 0; i < items.length; i++) {
         List<dynamic> row = [];
 
-        print(items[i].kod);
-
         if(columnsFromSettins.contains('naziv'))
           row.add(items[i].nazivArtikla);
         if(columnsFromSettins.contains('barkod'))
@@ -80,17 +78,54 @@ class DataExportService {
   }
 
   Future<bool> exportExcel(List<ListItem> items, String filename) async {    
-    try {
+    // try {
+      _settings = await _appSettingsService.getSettings();
+      var columnsFromSettings = _settings!.exportDataFields!.split(',').toList();
+      print(columnsFromSettings);
+
       final Workbook workbook = Workbook();
       final Worksheet sheet = workbook.worksheets[0];
-      sheet.getRangeByName('A1').setText('Barkod');
-      sheet.getRangeByName('B1').setText('Šifra');
-      sheet.getRangeByName('C1').setText('Količina');
+
+      var columnMappings = <String, String> {};
+      var columnIndex = 65;
+      if(columnsFromSettings.contains('barkod')) {
+        sheet.getRangeByName(String.fromCharCode(columnIndex) + '1').setText('Barkod');
+        columnMappings["barkod"] = String.fromCharCode(columnIndex);
+        columnIndex++;
+      }
+
+      if(columnsFromSettings.contains('šifra')) {
+        sheet.getRangeByName(String.fromCharCode(columnIndex) + '1').setText('Šifra');
+        columnMappings["kod"] = String.fromCharCode(columnIndex);
+        columnIndex++;
+      }
+
+      if(columnsFromSettings.contains('količina')) {
+        sheet.getRangeByName(String.fromCharCode(columnIndex) + '1').setText('Količina');
+        columnMappings["kolicina"] = String.fromCharCode(columnIndex);
+        columnIndex++;
+      }
+
+      if(columnsFromSettings.contains('naziv')) {
+        sheet.getRangeByName(String.fromCharCode(columnIndex) + '1').setText('Naziv');
+        columnMappings["naziv"] = String.fromCharCode(columnIndex);
+        columnIndex++;
+      }
+
+      if(columnsFromSettings.contains('jedinica mjere')) {
+        sheet.getRangeByName(String.fromCharCode(columnIndex) + '1').setText('Jedinica mjere');
+        columnMappings["jedinicaMjere"] = String.fromCharCode(columnIndex);
+        columnIndex++;
+      }
+
+      
 
       for(var i=0; i<items.length; i++) {
-        sheet.getRangeByName('A' + (i+2).toString()).setText(items[i].barkod);
-        sheet.getRangeByName('B' + (i+2).toString()).setText(items[i].kod);
-        sheet.getRangeByName('C' + (i+2).toString()).setText(items[i].kolicina.toString());
+        if(columnMappings["barkod"] != null) sheet.getRangeByName(columnMappings["barkod"]! + (i+2).toString()).setText(items[i].barkod);
+        if(columnMappings["kod"] != null) sheet.getRangeByName(columnMappings["kod"]! + (i+2).toString()).setText(items[i].kod);
+        if(columnMappings["kolicina"] != null) sheet.getRangeByName(columnMappings["kolicina"]! + (i+2).toString()).setText(items[i].kolicina.toString());
+        if(columnMappings["naziv"] != null) sheet.getRangeByName(columnMappings["naziv"]! + (i+2).toString()).setText(items[i].naziv.toString());
+        if(columnMappings["jedinicaMjere"] != null) sheet.getRangeByName(columnMappings["jedinicaMjere"]! + (i+2).toString()).setText(items[i].jedinicaMjere.toString());
       }
       
       final List<int> bytes = workbook.saveAsStream();
@@ -103,11 +138,11 @@ class DataExportService {
       await file.writeAsBytes(bytes, flush: true);
       await _openFile('$path/$filename.xlsx');
       return true;
-    }
-    catch(exception) {
-      print(exception);
-      return false;
-    }
+    // }
+    // catch(exception) {
+    //   print(exception);
+    //   return false;
+    // }
   }
   
   Future<bool> exportRestApi(List<ListItem> items) async {
